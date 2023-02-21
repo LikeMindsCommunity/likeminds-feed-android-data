f1244cc41a1be110e53483f6073d71d09e658585
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2023-02-21 12:59:15 +0530

    removed unnecessary keys from Community

--- internalsdk/src/main/java/com/likeminds/internalsdk/sdk/model/_Community_.kt
+++ internalsdk/src/main/java/com/likeminds/internalsdk/sdk/model/_Community_.kt
@@ -15,13 +15,7 @@ class _Community_ private constructor(
     @SerializedName("members_count")
     var membersCount: Int?,
     @SerializedName("updated_at")
-    var updatedAt: String?,
-    @SerializedName("created_by")
-    var createdBy: String?,
-    @SerializedName("managed_by")
-    var managedBy: String?,
-    @SerializedName("menu")
-    var menu: List<String>?
+    var updatedAt: String?
 ) : Parcelable {
 
     class Builder {
@@ -30,29 +24,19 @@ class _Community_ private constructor(
         private var imageUrl: String? = null
         private var membersCount: Int? = null
         private var updatedAt: String? = null
-        private var createdBy: String? = null
-        private var managedBy: String? = null
-        private var menu: List<String>? = null
 
         fun id(id: String) = apply { this.id = id }
         fun name(name: String) = apply { this.name = name }
         fun imageUrl(imageUrl: String?) = apply { this.imageUrl = imageUrl }
         fun membersCount(membersCount: Int?) = apply { this.membersCount = membersCount }
         fun updatedAt(updatedAt: String?) = apply { this.updatedAt = updatedAt }
-        fun createdBy(createdBy: String?) = apply { this.createdBy = createdBy }
-        fun managedBy(managedBy: String?) = apply { this.managedBy = managedBy }
-        fun menu(menu: List<String>?) = apply { this.menu = menu }
-
         //
         fun build() = _Community_(
             id,
             name,
             imageUrl,
             membersCount,
-            updatedAt,
-            createdBy,
-            managedBy,
-            menu,
+            updatedAt
         )
     }
 
@@ -61,7 +45,5 @@ class _Community_ private constructor(
             .name(name)
             .imageUrl(imageUrl)
             .updatedAt(updatedAt)
-            .createdBy(createdBy)
-            .managedBy(managedBy)
     }
 }
