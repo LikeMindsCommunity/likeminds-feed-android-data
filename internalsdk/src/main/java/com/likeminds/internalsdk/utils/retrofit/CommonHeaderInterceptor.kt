8ad9a9d75f002c57c367f9f5e98ad45b85cbae2f
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2023-03-27 16:34:03 +0530

    changed version code to 9999

--- internalsdk/src/main/java/com/likeminds/internalsdk/utils/retrofit/CommonHeaderInterceptor.kt
+++ internalsdk/src/main/java/com/likeminds/internalsdk/utils/retrofit/CommonHeaderInterceptor.kt
@@ -6,6 +6,7 @@ import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
+// TODO: change version code
 class CommonHeaderInterceptor @Inject constructor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val requestBuilder = chain.request().newBuilder()
