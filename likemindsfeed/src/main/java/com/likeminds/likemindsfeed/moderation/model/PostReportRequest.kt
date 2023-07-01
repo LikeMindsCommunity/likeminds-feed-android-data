6717a53aa8743222690d81334d33c038310af5bc
Ishaan Jain <Ishaan.jain@likeminds.community>
2023-07-01 21:08:38 +0530

    changed: PostRequestRequest for entityCreatorId to uuid

--- likemindsfeed/src/main/java/com/likeminds/likemindsfeed/moderation/model/PostReportRequest.kt
+++ likemindsfeed/src/main/java/com/likeminds/likemindsfeed/moderation/model/PostReportRequest.kt
@@ -2,7 +2,7 @@ package com.likeminds.likemindsfeed.moderation.model
 
 class PostReportRequest private constructor(
     var entityId: String,
-    var entityCreatorId: String,
+    var uuid: String,
     var entityType: Int,
     var tagId: Int,
     var reason: String?
@@ -10,14 +10,14 @@ class PostReportRequest private constructor(
 
     class Builder {
         private var entityId: String = ""
-        private var entityCreatorId: String = ""
+        private var uuid: String = ""
         private var entityType: Int = -1
         private var tagId: Int = -1
         private var reason: String? = null
 
         fun entityId(entityId: String) = apply { this.entityId = entityId }
         fun entityCreatorId(entityCreatorId: String) =
-            apply { this.entityCreatorId = entityCreatorId }
+            apply { this.uuid = entityCreatorId }
 
         fun entityType(entityType: Int) = apply { this.entityType = entityType }
         fun tagId(tagId: Int) = apply { this.tagId = tagId }
@@ -25,7 +25,7 @@ class PostReportRequest private constructor(
 
         fun build() = PostReportRequest(
             entityId,
-            entityCreatorId,
+            uuid,
             entityType,
             tagId,
             reason
@@ -34,7 +34,7 @@ class PostReportRequest private constructor(
 
     fun toBuilder(): Builder {
         return Builder().entityId(entityId)
-            .entityCreatorId(entityCreatorId)
+            .entityCreatorId(uuid)
             .entityType(entityType)
             .tagId(tagId)
             .reason(reason)
