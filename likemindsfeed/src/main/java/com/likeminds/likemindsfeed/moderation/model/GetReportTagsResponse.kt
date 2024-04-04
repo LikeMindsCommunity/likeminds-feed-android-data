db8728e111ad36c9db375bf3269a64ded6ab9a5a
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2024-04-04 21:11:55 +0530

    changed var to val

--- likemindsfeed/src/main/java/com/likeminds/likemindsfeed/moderation/model/GetReportTagsResponse.kt
+++ likemindsfeed/src/main/java/com/likeminds/likemindsfeed/moderation/model/GetReportTagsResponse.kt
@@ -1,10 +1,10 @@
 package com.likeminds.likemindsfeed.moderation.model
 
-data class GetReportTagsResponse constructor(
-    var tags: List<ReportTag>
+data class GetReportTagsResponse(
+    val tags: List<ReportTag>
 )
 
 data class ReportTag(
-    var id: Int,
-    var name: String
+    val id: Int,
+    val name: String
 )
