61af239399ded03256d9bef494c9ac9a9d09d7bf
Ishaan Jain <ishaan.jain@likeminds.community>
2025-01-23 17:51:06 +0530

    feat: added chucker for testing

--- internalsdk/src/main/java/com/likeminds/internalsdk/di/modules/NetworkModule.kt
+++ internalsdk/src/main/java/com/likeminds/internalsdk/di/modules/NetworkModule.kt
@@ -1,5 +1,7 @@
 package com.likeminds.internalsdk.di.modules
 
+import android.content.Context
+import com.chuckerteam.chucker.api.*
 import com.likeminds.internalsdk.sdk.TokenAuthenticator
 import com.likeminds.internalsdk.utils.retrofit.CommonHeaderInterceptor
 import com.likeminds.internalsdk.utils.retrofit.model.BaseUrl
@@ -25,8 +27,7 @@ class NetworkModule {
         loggingInterceptor: HttpLoggingInterceptor,
         commonHeaderInterceptor: CommonHeaderInterceptor,
         tokenAuthenticator: TokenAuthenticator,
-//        sentryOkHttpInterceptor: SentryOkHttpInterceptor,
-//        chuckerInterceptor: ChuckerInterceptor
+        chuckerInterceptor: ChuckerInterceptor
     ): OkHttpClient {
         val clientBuilder = OkHttpClient.Builder()
             .readTimeout(30L, TimeUnit.SECONDS)
@@ -35,8 +36,7 @@ class NetworkModule {
         clientBuilder.authenticator(tokenAuthenticator)
         clientBuilder.addInterceptor(loggingInterceptor)
         clientBuilder.addInterceptor(commonHeaderInterceptor)
-//        clientBuilder.addInterceptor(sentryOkHttpInterceptor)
-//        clientBuilder.addInterceptor(chuckerInterceptor)
+        clientBuilder.addInterceptor(chuckerInterceptor)
 
         return clientBuilder.build()
     }
@@ -49,19 +49,13 @@ class NetworkModule {
         return httpLoggingInterceptor
     }
 
-//    @Provides
-//    @Singleton
-//    fun provideSentryInterceptor(): SentryOkHttpInterceptor {
-//        return SentryOkHttpInterceptor()
-//    }
-
-//    @Provides
-//    @Singleton
-//    fun provideChuckInterceptor(context: Context): ChuckerInterceptor {
-//        val collector = ChuckerCollector(context, true, RetentionManager.Period.ONE_WEEK)
-//        return ChuckerInterceptor.Builder(context)
-//            .collector(collector)
-//            .alwaysReadResponseBody(false)
-//            .build()
-//    }
+    @Provides
+    @Singleton
+    fun provideChuckInterceptor(context: Context): ChuckerInterceptor {
+        val collector = ChuckerCollector(context, true, RetentionManager.Period.ONE_WEEK)
+        return ChuckerInterceptor.Builder(context)
+            .collector(collector)
+            .alwaysReadResponseBody(false)
+            .build()
+    }
 }
